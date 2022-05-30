2a1ac4cb7f9e744b1f1cfaacf76e03b56e6a526b
sheddyranking <shedyisaac@gmail.com>
2022-05-30 14:01:10 +0100

    SQL Data Exploration

--- 1.SQL Data Exploration/Data_Exploration.sql
+++ 1.SQL Data Exploration/Data_Exploration.sql
@@ -46,13 +46,15 @@ order by PercentagePopulationInfected DESC
 
 SELECT location, MAX(cast(total_deaths as int))  AS TotalDeathsCount
 FROM DAPortfolioProject..CovidDeaths
--- WHERE location LIKE '%States%
-WHERE continent IS NOT NULL
+--WHERE location LIKE '%States%
+WHERE continent IS NOT NULL 
 GROUP BY location 
 order by TotalDeathsCount DESC
 
 
 --- BREAKING IT DOWN TO CONTINENT 
+
+---- Showing the Continent with the Highest DeathCount per Population
 
 SELECT continent, MAX(cast(total_deaths as int))  AS TotalDeathsCount
 FROM DAPortfolioProject..CovidDeaths
@@ -60,3 +62,82 @@ FROM DAPortfolioProject..CovidDeaths
 WHERE continent IS NOT NULL
 GROUP BY continent 
 order by TotalDeathsCount DESC
+
+
+--- GLOBAL NUMBERING 
+
+SELECT  date, SUM(new_cases) AS Total_cases, SUM(cast(new_deaths as int)) as Total_deaths, SUM(cast(new_deaths as int))/SUM(new_cases)*100 AS DeathPercentage
+
+FROM DAPortfolioProject..CovidDeaths
+WHERE continent IS NOT NULL
+GROUP BY date
+ORDER BY 1,2
+
+
+--- GLOBAL TOTAL NUMBER
+
+SELECT SUM(new_cases) AS Total_cases, SUM(cast(new_deaths as int)) as Total_deaths, SUM(cast(new_deaths as int))/SUM(new_cases)*100 AS DeathPercentage
+
+FROM DAPortfolioProject..CovidDeaths
+WHERE continent IS NOT NULL
+--GROUP BY date
+ORDER BY 1,2
+
+
+
+-- PERFORMING JOIN WITH VACCINATION TABLE ON THE BASE OF (location and date)
+
+SELECT * 
+FROM DAPortfolioProject..CovidDeaths dea
+JOIN DAPortfolioProject..CovidVaccinations vac
+	ON dea.location = vac.location
+	AND dea.date = vac.date
+
+
+-- LOOKING AT TOTAL POPULATIONS VS VACCINATIONS
+
+SELECT dea.continent, dea.location, dea.date, dea.population, vac.new_vaccinations
+
+FROM DAPortfolioProject..CovidDeaths dea
+JOIN DAPortfolioProject..CovidVaccinations vac
+	ON dea.location = vac.location
+	AND dea.date = vac.date
+WHERE dea.continent IS NOT NULL
+ORDER BY 2,3
+
+
+---CREATING NEW_VACCINATION COUNT COLUMNS(RollingPeopleVaccinated)
+
+SELECT dea.continent, dea.location, dea.date, dea.population, vac.new_vaccinations, 
+SUM(cast(vac.new_vaccinations as int)) OVER (PARTITION BY dea.location ORDER BY dea.location, dea.date) as RollingPeopleVaccinated --CREATING NEW_VACCINATION COUNT COLUMN
+
+FROM DAPortfolioProject..CovidDeaths dea
+JOIN DAPortfolioProject..CovidVaccinations vac
+	ON dea.location = vac.location
+	AND dea.date = vac.date
+WHERE dea.continent IS NOT NULL
+ORDER BY 2,3
+
+-- CREATING A CTE(Common Table Expression), So we can use created RollingPeopleVaccinated Column do other Calculations.
+
+WITH PopvsVac (continent, location, date, population, new_vaccination, RollingPeopleVaccinated)
+AS
+(
+SELECT dea.continent, dea.location, dea.date, dea.population, vac.new_vaccinations, 
+SUM(cast(vac.new_vaccinations as int)) OVER (PARTITION BY dea.location ORDER BY dea.location, dea.date) as RollingPeopleVaccinated --CREATING NEW_VACCINATION COUNT COLUMN
+
+FROM DAPortfolioProject..CovidDeaths dea
+JOIN DAPortfolioProject..CovidVaccinations vac
+	ON dea.location = vac.location
+	AND dea.date = vac.date
+WHERE dea.continent IS NOT NULL
+--ORDER BY 2,3
+)
+SELECT *,  (RollingPeopleVaccinated/population)*100  
+FROM PopvsVac  --Highlight the Whole Query and Execute
+
+
+--- NOW MAKING CALCULATOINS WITH RollingPeopleVaccinated Column to Know Percentage Poluation Vaccinated by Country.
+
+ 
+
