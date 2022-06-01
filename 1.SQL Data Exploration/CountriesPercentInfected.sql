9b9e102feffaecd5ac2b8ccd7b24d6980c5fe8c2
sheddyranking <shedyisaac@gmail.com>
2022-06-01 12:56:18 +0100

    CountriesPercentInfected

--- 1.SQL Data Exploration/CountriesPercentInfected.sql
+++ 1.SQL Data Exploration/CountriesPercentInfected.sql
@@ -1,6 +1,2 @@
-/****** Script for SelectTopNRows command from SSMS  ******/
-SELECT TOP (1000) [location]
-      ,[population]
-      ,[HighestInfectionCount]
-      ,[CountriesPercentInfected]
-  FROM [DAPortfolioProject].[dbo].[CountriesPercentInfected]
+
+SELECT * FROM CountriesPercentInfected
