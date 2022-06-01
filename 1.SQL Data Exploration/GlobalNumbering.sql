2f508054bd4937e030ba39fb935ddcb61b42fccf
sheddyranking <shedyisaac@gmail.com>
2022-06-01 13:00:05 +0100

    Global Numbering

--- 1.SQL Data Exploration/GlobalNumbering.sql
+++ 1.SQL Data Exploration/GlobalNumbering.sql
@@ -1,6 +1,2 @@
-/****** Script for SelectTopNRows command from SSMS  ******/
-SELECT TOP (1000) [date]
-      ,[Total_cases]
-      ,[Total_deaths]
-      ,[GlobalNumbering]
-  FROM [DAPortfolioProject].[dbo].[GlobalNumbering]
+
+SELECT * FROM  GlobalNumbering
