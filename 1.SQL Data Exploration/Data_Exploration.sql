4693d499a6fe79931da2e6001a96664cf70c1b0c
sheddyranking <shedyisaac@gmail.com>
2022-06-01 11:52:39 +0100

    SQL Data Exploration

--- 1.SQL Data Exploration/Data_Exploration.sql
+++ 1.SQL Data Exploration/Data_Exploration.sql
@@ -21,6 +21,15 @@ SELECT location, date, total_cases, total_deaths, (total_deaths/total_cases)*100
 FROM DAPortfolioProject..CovidDeaths
 WHERE location LIKE '%States%' and  continent IS NOT NULL
 order by 1,2
+ 
+CREATE VIEW DeathPercentage as --Create View to store query in table 
+SELECT location, date, total_cases, total_deaths, (total_deaths/total_cases)*100 AS DeathPercentage
+FROM DAPortfolioProject..CovidDeaths
+WHERE location LIKE '%States%' and  continent IS NOT NULL
+--order by 1,2
+ 
+
+
 
 --- LOOKING AT TOTAL CASES VS POPULATIONS 
 --- Shows the Percentage of those who got Covid
@@ -31,15 +40,35 @@ FROM DAPortfolioProject..CovidDeaths
 WHERE continent IS NOT NULL
 order by 1,2
 
+CREATE VIEW PercentagePopulationInfected as --Creating view
+SELECT location, date, population, total_cases, (total_cases/population)*100 AS   PercentagePopulationInfected
+FROM DAPortfolioProject..CovidDeaths
+-- WHERE location LIKE '%States%'
+WHERE continent IS NOT NULL
+--order by 1,2
+
+
+
+
 
 --- LOOKING AT COUNTRIES WITH THE HIGHEST INFECTION RATE COMPARE TO POPULATION
 
-SELECT location, population, MAX(total_cases) AS HighestInfectionCount, MAX((total_cases/population))*100 AS PercentagePopulationInfected
+SELECT location, population, MAX(total_cases) AS HighestInfectionCount, MAX((total_cases/population))*100 AS CountriesPercentInfected
 FROM DAPortfolioProject..CovidDeaths
 -- WHERE location LIKE '%States%'
 WHERE continent IS NOT NULL
 GROUP BY location, population
-order by PercentagePopulationInfected DESC
+order by CountriesPercentInfected DESC
+
+CREATE VIEW CountriesPercentInfected  as --Creating View
+SELECT location, population, MAX(total_cases) AS HighestInfectionCount, MAX((total_cases/population))*100 AS CountriesPercentInfected
+FROM DAPortfolioProject..CovidDeaths
+-- WHERE location LIKE '%States%'
+WHERE continent IS NOT NULL
+GROUP BY location, population
+--order by CountriesPercentInfected DESC
+
+ 
 
 
 --LOOKING AT COUNTRIES WITH HIGHEST DEATH COUNT PER POPULATION
@@ -51,37 +80,72 @@ WHERE continent IS NOT NULL
 GROUP BY location 
 order by TotalDeathsCount DESC
 
+CREATE VIEW TotalDeathsCount as --Creating view 
+SELECT location, MAX(cast(total_deaths as int))  AS TotalDeathsCount
+FROM DAPortfolioProject..CovidDeaths
+--WHERE location LIKE '%States%
+WHERE continent IS NOT NULL 
+GROUP BY location 
+--order by TotalDeathsCount DESC
+
+
+
 
 --- BREAKING IT DOWN TO CONTINENT 
 
 ---- Showing the Continent with the Highest DeathCount per Population
 
-SELECT continent, MAX(cast(total_deaths as int))  AS TotalDeathsCount
+SELECT continent, MAX(cast(total_deaths as int))  AS TotalDeathsCountPerContinent
 FROM DAPortfolioProject..CovidDeaths
 
 WHERE continent IS NOT NULL
 GROUP BY continent 
-order by TotalDeathsCount DESC
+order by TotalDeathsCountPerContinent DESC
+
+CREATE VIEW TotalDeathsCountPerContinent as -- Creating view
+SELECT continent, MAX(cast(total_deaths as int))  AS TotalDeathsCountPerContinent
+FROM DAPortfolioProject..CovidDeaths
+WHERE continent IS NOT NULL
+GROUP BY continent 
+--order by TotalDeathsCountPerContinent DESC
+
+ 
 
 
 --- GLOBAL NUMBERING 
 
-SELECT  date, SUM(new_cases) AS Total_cases, SUM(cast(new_deaths as int)) as Total_deaths, SUM(cast(new_deaths as int))/SUM(new_cases)*100 AS DeathPercentage
+SELECT  date, SUM(new_cases) AS Total_cases, SUM(cast(new_deaths as int)) as Total_deaths, SUM(cast(new_deaths as int))/SUM(new_cases)*100 AS GlobalNumbering
 
 FROM DAPortfolioProject..CovidDeaths
 WHERE continent IS NOT NULL
 GROUP BY date
 ORDER BY 1,2
 
+CREATE VIEW  GlobalNumbering as 
+SELECT  date, SUM(new_cases) AS Total_cases, SUM(cast(new_deaths as int)) as Total_deaths, SUM(cast(new_deaths as int))/SUM(new_cases)*100 AS GlobalNumbering
+
+FROM DAPortfolioProject..CovidDeaths
+WHERE continent IS NOT NULL
+GROUP BY date
+--ORDER BY 1,2
+
+
 
 --- GLOBAL TOTAL NUMBER
 
-SELECT SUM(new_cases) AS Total_cases, SUM(cast(new_deaths as int)) as Total_deaths, SUM(cast(new_deaths as int))/SUM(new_cases)*100 AS DeathPercentage
+SELECT SUM(new_cases) AS Total_cases, SUM(cast(new_deaths as int)) as Total_deaths, SUM(cast(new_deaths as int))/SUM(new_cases)*100 AS TotalGlobalNumbering
 
 FROM DAPortfolioProject..CovidDeaths
 WHERE continent IS NOT NULL
 --GROUP BY date
-ORDER BY 1,2
+--ORDER BY 1,2
+
+CREATE VIEW TotalGlobalNumbering as 
+SELECT SUM(new_cases) AS Total_cases, SUM(cast(new_deaths as int)) as Total_deaths, SUM(cast(new_deaths as int))/SUM(new_cases)*100 AS TotalGlobalNumbering
+FROM DAPortfolioProject..CovidDeaths
+WHERE continent IS NOT NULL
+--GROUP BY date
+--ORDER BY 1,2
 
 
 
@@ -105,6 +169,18 @@ JOIN DAPortfolioProject..CovidVaccinations vac
 WHERE dea.continent IS NOT NULL
 ORDER BY 2,3
 
+CREATE VIEW TotalPopulationVsVaccinated as  --Creating View
+SELECT dea.continent, dea.location, dea.date, dea.population, vac.new_vaccinations
+
+FROM DAPortfolioProject..CovidDeaths dea
+JOIN DAPortfolioProject..CovidVaccinations vac
+	ON dea.location = vac.location
+	AND dea.date = vac.date
+WHERE dea.continent IS NOT NULL
+--ORDER BY 2,3
+
+ 
+
 
 ---CREATING NEW_VACCINATION COUNT COLUMNS(RollingPeopleVaccinated)
 
@@ -117,6 +193,19 @@ JOIN DAPortfolioProject..CovidVaccinations vac
 	AND dea.date = vac.date
 WHERE dea.continent IS NOT NULL
 ORDER BY 2,3
+
+CREATE VIEW RollingPeopleVaccinated as 
+SELECT dea.continent, dea.location, dea.date, dea.population, vac.new_vaccinations, 
+SUM(cast(vac.new_vaccinations as int)) OVER (PARTITION BY dea.location ORDER BY dea.location, dea.date) as RollingPeopleVaccinated --CREATING NEW_VACCINATION COUNT COLUMN
+
+FROM DAPortfolioProject..CovidDeaths dea
+JOIN DAPortfolioProject..CovidVaccinations vac
+	ON dea.location = vac.location
+	AND dea.date = vac.date
+WHERE dea.continent IS NOT NULL
+--ORDER BY 2,3
+
+
 
 -- CREATING A CTE(Common Table Expression), So we can use created RollingPeopleVaccinated Column do other Calculations.
 
@@ -133,10 +222,51 @@ JOIN DAPortfolioProject..CovidVaccinations vac
 WHERE dea.continent IS NOT NULL
 --ORDER BY 2,3
 )
-SELECT *,  (RollingPeopleVaccinated/population)*100  --- NOW MAKING CALCULATOINS WITH RollingPeopleVaccinated Column to Know Per% Polulation Vaccinated by Country.
+SELECT *,  (RollingPeopleVaccinated/population)*100 AS RPVcalculate  --- NOW MAKING CALCULATOINS WITH RollingPeopleVaccinated Column to Know Per% Polulation Vaccinated by Country.
 FROM PopvsVac  --Highlight the Whole Query and Execute
 
 
+-- TEMP TABLE
+DROP TABLE if exists #PercentPopulationVaccinated
+CREATE TABLE #PercentPopulationVaccinated
+(
+Continent nvarchar(255),
+Location nvarchar(255),
+Date datetime,
+Population numeric,
+New_vaccinations numeric,
+RollingPeopleVaccinated numeric
+)
 
- 
+INSERT INTO #PercentPopulationVaccinated
+SELECT dea.continent, dea.location, dea.date, dea.population, vac.new_vaccinations, 
+SUM(cast(vac.new_vaccinations as int)) OVER (PARTITION BY dea.location ORDER BY dea.location, dea.date) as RollingPeopleVaccinated --CREATING NEW_VACCINATION COUNT COLUMN
 
+FROM DAPortfolioProject..CovidDeaths dea
+JOIN DAPortfolioProject..CovidVaccinations vac
+	ON dea.location = vac.location
+	AND dea.date = vac.date
+--WHERE dea.continent IS NOT NULL
+
+SELECT *,  (RollingPeopleVaccinated/population)*100 AS  RVPTempTableCalculate
+FROM #PercentPopulationVaccinated
+
+
+
+-- CREATING VIEWS  FOR DATA VISUALIZATIONS 
+
+CREATE VIEW PercentPopulationVaccinated as 
+SELECT dea.continent, dea.location, dea.date, dea.population, vac.new_vaccinations, 
+SUM(cast(vac.new_vaccinations as int)) OVER (PARTITION BY dea.location ORDER BY dea.location, dea.date) as RollingPeopleVaccinated 
+
+FROM DAPortfolioProject..CovidDeaths dea
+JOIN DAPortfolioProject..CovidVaccinations vac
+	ON dea.location = vac.location
+	AND dea.date = vac.date
+WHERE dea.continent IS NOT NULL
+--ORDER BY 2,3
+
+--- YOU CAN SELECT THE VIEWS BECAUSE IT IS NOW A TABLE THAT HAS BEEN CREATED 
+
+SELECT *
+FROM PercentPopulationVaccinated
