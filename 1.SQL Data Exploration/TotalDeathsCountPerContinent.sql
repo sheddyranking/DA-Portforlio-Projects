692ac67dfb7f53d8325ec4402e7e7e390774471a
sheddyranking <shedyisaac@gmail.com>
2022-06-01 12:19:41 +0100

    TotalDeathCountPerContinent

---
+++ 1.SQL Data Exploration/TotalDeathsCountPerContinent.sql
@@ -0,0 +1,4 @@
+/****** Script for SelectTopNRows command from SSMS  ******/
+SELECT TOP (1000) [continent]
+      ,[TotalDeathsCountPerContinent]
+  FROM [DAPortfolioProject].[dbo].[TotalDeathsCountPerContinent]
